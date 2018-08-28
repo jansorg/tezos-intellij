b8286e16013b93d15caf841956347a71adbc9c41
jansorg <github@joachim-ansorg.de>
2018-08-28 13:16:32 +0200

    Enhance syntax highlighting Add settings page to configurat syntax highlighting of the michelson language

---
+++ src/main/kotlin/com/tezos/lang/michelson/highlighting/MichelsonHighlightingAnnotator.kt
@@ -0,0 +1,7 @@
+package com.tezos.lang.michelson.highlighting
+
+/**
+ * @author jansorg
+ */
+class MichelsonHighlightingAnnotator {
+}
--- src/main/kotlin/com/tezos/lang/michelson/highlighting/MichelsonSyntaxHighlighter.kt
+++ src/main/kotlin/com/tezos/lang/michelson/highlighting/MichelsonSyntaxHighlighter.kt
@@ -16,56 +16,65 @@ import java.util.*
  * @author jansorg
  */
 class MichelsonSyntaxHighlighter : SyntaxHighlighterBase() {
-    private companion object {
-        private val LINE_COMMENT = TextAttributesKey.createTextAttributesKey("MI.LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
-        private val BLOCK_COMMENT = TextAttributesKey.createTextAttributesKey("MI.BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT)
+    internal companion object {
+        val LINE_COMMENT = TextAttributesKey.createTextAttributesKey("MI.LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+        val BLOCK_COMMENT = TextAttributesKey.createTextAttributesKey("MI.BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT)
 
-        private val SECTION_NAME = TextAttributesKey.createTextAttributesKey("MI.SECTION_NAME", DefaultLanguageHighlighterColors.KEYWORD)
-        private val TYPE_NAME = TextAttributesKey.createTextAttributesKey("MI.TYPE_NAME", DefaultLanguageHighlighterColors.CLASS_REFERENCE)
-        private val TYPE_NAME_COMPARABLE = TextAttributesKey.createTextAttributesKey("MI.TYPE_NAME_COMPARABLE", TYPE_NAME)
-        private val TAG = TextAttributesKey.createTextAttributesKey("MI.TAG", DefaultLanguageHighlighterColors.STATIC_METHOD)
-        private val INSTRUCTION = TextAttributesKey.createTextAttributesKey("MI.INSTRUCTION", DefaultLanguageHighlighterColors.KEYWORD)
-        private val MACRO = TextAttributesKey.createTextAttributesKey("MI.MACRO", DefaultLanguageHighlighterColors.KEYWORD)
+        val SECTION_NAME = TextAttributesKey.createTextAttributesKey("MI.SECTION_NAME", DefaultLanguageHighlighterColors.KEYWORD)
+        val TAG = TextAttributesKey.createTextAttributesKey("MI.TAG", DefaultLanguageHighlighterColors.STATIC_METHOD)
+
+        val INSTRUCTION = TextAttributesKey.createTextAttributesKey("MI.INSTRUCTION", DefaultLanguageHighlighterColors.FUNCTION_CALL)
+        val MACRO = TextAttributesKey.createTextAttributesKey("MI.MACRO", INSTRUCTION)
+
+        val TYPE_NAME = TextAttributesKey.createTextAttributesKey("MI.TYPE_NAME", DefaultLanguageHighlighterColors.CLASS_REFERENCE)
+        val TYPE_NAME_COMPARABLE = TextAttributesKey.createTextAttributesKey("MI.TYPE_NAME_COMPARABLE", TYPE_NAME)
+
+        val ANNOTATION = TextAttributesKey.createTextAttributesKey("MI.ANNOTATION", DefaultLanguageHighlighterColors.METADATA)
+        val TYPE_ANNOTATION = TextAttributesKey.createTextAttributesKey("MI.ANNOTATION_TYPE", ANNOTATION)
+        val VARIABLE_ANNOTATION = TextAttributesKey.createTextAttributesKey("MI.ANNOTATION_VAR", ANNOTATION)
+        val FIELD_ANNOTATION = TextAttributesKey.createTextAttributesKey("MI.ANNOTATION_FIELD", ANNOTATION)
 
         // literal
-        private val INT = TextAttributesKey.createTextAttributesKey("MI.INT", DefaultLanguageHighlighterColors.NUMBER)
-        private val STRING = TextAttributesKey.createTextAttributesKey("MI.STRING", DefaultLanguageHighlighterColors.STRING)
-        private val BYTE = TextAttributesKey.createTextAttributesKey("MI.BYTE", DefaultLanguageHighlighterColors.CONSTANT)
-        private val OTHER_LITERAL = TextAttributesKey.createTextAttributesKey("MI.OTHER_LITERAL", DefaultLanguageHighlighterColors.PREDEFINED_SYMBOL)
-        private val BOOLEAN_LITERAL = TextAttributesKey.createTextAttributesKey("MI.BOOL_LITERAL", OTHER_LITERAL)
-        private val UNIT_LITERAL = TextAttributesKey.createTextAttributesKey("MI.UNIT_LITERAL", OTHER_LITERAL)
+        val INT_LITERAL = TextAttributesKey.createTextAttributesKey("MI.INT", DefaultLanguageHighlighterColors.NUMBER)
+        val STRING_LITERAL = TextAttributesKey.createTextAttributesKey("MI.STRING", DefaultLanguageHighlighterColors.STRING)
+        val BYTE_LITERAL = TextAttributesKey.createTextAttributesKey("MI.BYTE", DefaultLanguageHighlighterColors.CONSTANT)
+        val BOOLEAN_LITERAL = TextAttributesKey.createTextAttributesKey("MI.BOOL_LITERAL", DefaultLanguageHighlighterColors.PREDEFINED_SYMBOL)
+        val UNIT_LITERAL = TextAttributesKey.createTextAttributesKey("MI.UNIT_LITERAL", DefaultLanguageHighlighterColors.PREDEFINED_SYMBOL)
 
         // generic symbols
-        private val SEMI = TextAttributesKey.createTextAttributesKey("MI.SEMI", DefaultLanguageHighlighterColors.SEMICOLON)
-        private val PAREN = TextAttributesKey.createTextAttributesKey("MI.PAREN", DefaultLanguageHighlighterColors.PARENTHESES)
-        private val BRACES = TextAttributesKey.createTextAttributesKey("MI.PAREN", DefaultLanguageHighlighterColors.BRACES)
-    }
+        val SEMI = TextAttributesKey.createTextAttributesKey("MI.SEMI", DefaultLanguageHighlighterColors.SEMICOLON)
+        val PAREN = TextAttributesKey.createTextAttributesKey("MI.PAREN", DefaultLanguageHighlighterColors.PARENTHESES)
+        val BRACES = TextAttributesKey.createTextAttributesKey("MI.PAREN", DefaultLanguageHighlighterColors.BRACES)
 
-    private val keys = HashMap<IElementType, TextAttributesKey>()
+        // keys
+        private val keys = HashMap<IElementType, TextAttributesKey>()
+        init {
+            keys.put(TokenType.BAD_CHARACTER, HighlighterColors.BAD_CHARACTER)
 
-    init {
-        keys.put(TokenType.BAD_CHARACTER, HighlighterColors.BAD_CHARACTER)
+            keys.put(MichelsonTypes.COMMENT_LINE, LINE_COMMENT)
+            keys.put(MichelsonTypes.COMMENT_MULTI_LINE, BLOCK_COMMENT)
 
-        keys.put(MichelsonTypes.COMMENT_LINE, LINE_COMMENT)
-        keys.put(MichelsonTypes.COMMENT_MULTI_LINE, BLOCK_COMMENT)
+            keys.put(MichelsonTypes.SECTION_NAME, SECTION_NAME)
 
-        keys.put(MichelsonTypes.SECTION_NAME, SECTION_NAME)
+            keys.put(MichelsonTypes.TYPE_NAME, TYPE_NAME)
+            keys.put(MichelsonTypes.TYPE_NAME_COMPARABLE, TYPE_NAME_COMPARABLE)
+            keys.put(MichelsonTypes.TAG, TAG)
+            keys.put(MichelsonTypes.INSTRUCTION_TOKEN, INSTRUCTION)
+            fillMap(keys, MichelsonElementTypes.MACROS, MACRO)
 
-        keys.put(MichelsonTypes.TYPE_NAME, TYPE_NAME)
-        keys.put(MichelsonTypes.TYPE_NAME_COMPARABLE, TYPE_NAME_COMPARABLE)
-        keys.put(MichelsonTypes.TAG, TAG)
-        keys.put(MichelsonTypes.INSTRUCTION_TOKEN, INSTRUCTION)
-        fillMap(keys, MichelsonElementTypes.MACROS, MACRO)
+            keys.put(MichelsonTypes.ANNOTATION_TOKEN, ANNOTATION)
+            //fixme better handle different types of annotations in annotator
 
-        keys.put(MichelsonTypes.INT, INT)
-        keys.put(MichelsonTypes.BYTE, BYTE)
-        keys.put(MichelsonTypes.STRING, STRING)
-        keys.put(MichelsonTypes.UNIT, UNIT_LITERAL)
-        fillMap(keys, MichelsonElementTypes.BOOLEAN, BOOLEAN_LITERAL)
+            keys.put(MichelsonTypes.INT, INT_LITERAL)
+            keys.put(MichelsonTypes.BYTE, BYTE_LITERAL)
+            keys.put(MichelsonTypes.STRING, STRING_LITERAL)
+            keys.put(MichelsonTypes.UNIT, UNIT_LITERAL)
+            fillMap(keys, MichelsonElementTypes.BOOLEAN, BOOLEAN_LITERAL)
 
-        keys.put(MichelsonTypes.SEMI, SEMI)
-        fillMap(keys, MichelsonElementTypes.PARENTHESES, PAREN)
-        fillMap(keys, MichelsonElementTypes.BRACES, BRACES)
+            keys.put(MichelsonTypes.SEMI, SEMI)
+            fillMap(keys, MichelsonElementTypes.PARENTHESES, PAREN)
+            fillMap(keys, MichelsonElementTypes.BRACES, BRACES)
+        }
     }
 
     override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> = pack(keys.get(tokenType))
