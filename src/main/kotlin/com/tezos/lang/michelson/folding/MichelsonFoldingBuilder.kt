9fd4211c21b9a937d305aab61f7ff24164f61d9a
jansorg <github@joachim-ansorg.de>
2018-09-21 09:00:25 +0200

    Simple folding builder

---
+++ src/main/kotlin/com/tezos/lang/michelson/folding/MichelsonFoldingBuilder.kt
@@ -0,0 +1,51 @@
+package com.tezos.lang.michelson.folding
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilder
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.impl.source.tree.TreeUtil
+import com.intellij.psi.tree.TokenSet
+import com.tezos.lang.michelson.MichelsonTypes
+
+/**
+ * Folding builder for the Michelson language.
+ * @author jansorg
+ */
+class MichelsonFoldingBuilder : FoldingBuilder, DumbAware {
+    companion object {
+        private val foldedTypes = TokenSet.create(MichelsonTypes.BLOCK_INSTRUCTION)
+    }
+
+    override fun getPlaceholderText(node: ASTNode): String? {
+        return when {
+            node.elementType == MichelsonTypes.BLOCK_INSTRUCTION -> "{...}"
+            node.elementType == MichelsonTypes.CONTRACT -> "CONTRACT ..."
+            else -> null
+        }
+    }
+
+    override fun isCollapsedByDefault(node: ASTNode): Boolean = false
+
+    override fun buildFoldRegions(node: ASTNode, document: Document): Array<FoldingDescriptor> {
+        val result = ArrayList<FoldingDescriptor>()
+        buildRecursively(node, document, result)
+        return result.toTypedArray()
+    }
+
+    private fun buildRecursively(node: ASTNode, document: Document, result: MutableList<FoldingDescriptor>) {
+        if (node.elementType == MichelsonTypes.CONTRACT) {
+            val notTopLevel = TreeUtil.findParent(node, MichelsonTypes.CREATE_CONTRACT_INSTRUCTION) != null
+            if (notTopLevel) {
+                result += FoldingDescriptor(node, node.textRange)
+            }
+        } else if (foldedTypes.contains(node.elementType) && node.textLength > 2) {
+            result += FoldingDescriptor(node, node.textRange)
+        }
+
+        for (c in node.getChildren(null)) {
+            buildRecursively(c, document, result)
+        }
+    }
+}
