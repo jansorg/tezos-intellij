97e39897edfb59bd3c404864ef673d5f7f497be2
jansorg <github@joachim-ansorg.de>
2018-10-12 12:40:09 +0200

    wip

---
+++ src/main/kotlin/com/tezos/lang/michelson/editor/stack/StackAwareEditorProvider.kt
@@ -0,0 +1,40 @@
+package com.tezos.lang.michelson.editor.stack
+
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.fileEditor.FileEditorPolicy
+import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.fileEditor.FileEditorState
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VirtualFile
+import com.tezos.lang.michelson.lang.MichelsonFileType
+import org.jdom.Element
+
+/**
+ * @author jansorg
+ */
+class StackAwareEditorProvider : FileEditorProvider, DumbAware {
+    override fun getEditorTypeId(): String = "tezos.michelson"
+
+    override fun accept(project: Project, file: VirtualFile): Boolean {
+        return file.fileType == MichelsonFileType
+    }
+
+    override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR
+
+    override fun createEditor(project: Project, file: VirtualFile): FileEditor {
+        return MichelsonStackEditor(project, file)
+    }
+
+    override fun readState(sourceElement: Element, project: Project, file: VirtualFile): FileEditorState {
+        return FileEditorState.INSTANCE
+    }
+
+    override fun writeState(state: FileEditorState, project: Project, targetElement: Element) {
+    }
+
+    override fun disposeEditor(editor: FileEditor) {
+        Disposer.dispose(editor)
+    }
+}
