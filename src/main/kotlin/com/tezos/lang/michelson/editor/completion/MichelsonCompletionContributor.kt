721a1e4c44b57189e659959f92e2689ed659f660
jansorg <github@joachim-ansorg.de>
2018-11-23 10:44:06 +0100

    cleanup code

--- src/main/kotlin/com/tezos/lang/michelson/editor/completion/MichelsonCompletionContributor.kt
+++ src/main/kotlin/com/tezos/lang/michelson/editor/completion/MichelsonCompletionContributor.kt
@@ -4,6 +4,7 @@ import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.StandardPatterns
 import com.intellij.psi.TokenType
 import com.intellij.psi.tree.TokenSet
+import com.tezos.lang.michelson.MichelsonTypes
 import com.tezos.lang.michelson.MichelsonTypes.*
 import com.tezos.lang.michelson.editor.completion.provider.*
 
@@ -11,38 +12,22 @@ import com.tezos.lang.michelson.editor.completion.provider.*
  * @author jansorg
  */
 class MichelsonCompletionContributor : AbstractOriginalPosCompletionContributor() {
-    init {
-        // sections
-        extend(null, TOPLEVEL_PATTERN, SectionCompletion())
-
-        // instructions
-        val commandEndPattern = PlatformPatterns.psiElement().withElementType(TokenSet.create(LEFT_CURLY, SEMI))
-        val instructionPlace = StandardPatterns.or(
-                commandEndPattern,
-                PlatformPatterns.psiElement().afterLeaf(commandEndPattern),
-                PlatformPatterns.psiElement(INSTRUCTION_TOKEN).andNot(AFTER_ERROR_LEAF_SKIPPING_WS)
-        )
-        extendOriginal(null, instructionPlace, InstructionNameCompletion())
-
-        // types
-        val AFTER_INSTRUCTION_TOKEN_LEAF = PlatformPatterns.psiElement().afterLeaf(PATTERN_INSTRUCTION_TOKEN)
-        val AFTER_INSTRUCTION_ELEMENT = PlatformPatterns.psiElement().afterSibling(INSTRUCTION_ELEMENT_PATTERN)
+    private companion object {
+        val AFTER_INSTRUCTION_TOKEN_LEAF = PlatformPatterns.psiElement().afterLeaf(PATTERN_INSTRUCTION_TOKEN)!!
+        val AFTER_INSTRUCTION_ELEMENT = PlatformPatterns.psiElement().afterSibling(INSTRUCTION_ELEMENT_PATTERN)!!
         val SIMPLE_TYPE_PATTERN = PlatformPatterns.or(
-                DEBUG_FALSE,
                 PlatformPatterns.psiElement().inside(INSTRUCTION_ELEMENT_PATTERN)
                         .andNot(PATTERN_INSTRUCTION_TOKEN)
                         .andNot(PATTERN_INSIDE_DATA_ELEMENT)
                         .andNot(PlatformPatterns.psiElement().afterLeaf(PATTERN_TAG_TOKEN)),
                 AFTER_INSTRUCTION_TOKEN_LEAF.andNot(PATTERN_INSIDE_DATA_ELEMENT)
-        )
-        extendOriginal(null, SIMPLE_TYPE_PATTERN, SimpleTypeCompletion())
-        extendOriginal(null, LEFT_PAREN_PATTERN.and(SIMPLE_TYPE_PATTERN), NestedTypeCompletion())
+        )!!
 
-        // tags
-        val TAG_AFTER_INSTRUCTION_TOKEN = PlatformPatterns.psiElement().withElementType(TokenSet.create(TokenType.WHITE_SPACE, TAG_TOKEN, TAG))
-                .and(AFTER_INSTRUCTION_TOKEN_LEAF)
+        val TAG_AFTER_INSTRUCTION_TOKEN = PlatformPatterns.psiElement()
+                .withElementType(TokenSet.create(TokenType.WHITE_SPACE, TAG_TOKEN, MichelsonTypes.TAG))
+                .and(AFTER_INSTRUCTION_TOKEN_LEAF)!!
 
-        val tag = StandardPatterns.or(
+        val TAG = StandardPatterns.or(
                 // e.g. "PUSH |"
                 TAG_AFTER_INSTRUCTION_TOKEN,
                 // e.g. "PUSH | ..." where the parser did error recovery
@@ -57,11 +42,31 @@ class MichelsonCompletionContributor : AbstractOriginalPosCompletionContributor(
                 AFTER_ERROR_LEAF_SKIPPING_WS.beforeLeaf(PATTERN_INSTRUCTION_TOKEN),
                 // e.g. "PUSH int |Tr" (Tr is a tag here)
                 WHITESPACE_PATTERN.beforeLeaf(PATTERN_TAG_TOKEN)
-        )
-        extendOriginal(null, tag, SimpleTagCompletion())
+        )!!
 
-        val complexTag = PlatformPatterns.or(LEFT_PAREN_PATTERN, PlatformPatterns.psiElement(TAG_TOKEN).afterLeafSkipping(WHITESPACE_PATTERN, LEFT_PAREN_PATTERN))
-        extendOriginal(null, complexTag, NestedTagCompletion())
+        val COMPLEX_TAG = PlatformPatterns.or(LEFT_PAREN_PATTERN, PlatformPatterns.psiElement(TAG_TOKEN).afterLeafSkipping(WHITESPACE_PATTERN, LEFT_PAREN_PATTERN))!!
+    }
+
+    init {
+        // sections
+        extend(null, TOPLEVEL_PATTERN, SectionCompletion())
+
+        // instructions
+        val commandEndPattern = PlatformPatterns.psiElement().withElementType(TokenSet.create(LEFT_CURLY, SEMI))
+        val instructionPlace = StandardPatterns.or(
+                commandEndPattern,
+                PlatformPatterns.psiElement().afterLeaf(commandEndPattern),
+                PlatformPatterns.psiElement(INSTRUCTION_TOKEN).andNot(AFTER_ERROR_LEAF_SKIPPING_WS)
+        )
+        extendOriginal(null, instructionPlace, InstructionNameCompletion())
+
+        // types
+        extendOriginal(null, SIMPLE_TYPE_PATTERN, SimpleTypeCompletion())
+        extendOriginal(null, LEFT_PAREN_PATTERN.and(SIMPLE_TYPE_PATTERN), NestedTypeCompletion())
+
+        // tags
+        extendOriginal(null, TAG, SimpleTagCompletion())
+        extendOriginal(null, COMPLEX_TAG, NestedTagCompletion())
     }
 }
 
