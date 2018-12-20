67ba2cb7be3831e65e2aac131fff893263a949f3
jansorg <github@joachim-ansorg.de>
2018-12-20 15:31:24 +0100

    refactor tests, add lookup test

--- src/test/kotlin/com/tezos/lang/michelson/stackInfo/MichelsonStackInfoManagerImplTest.kt
+++ src/test/kotlin/com/tezos/lang/michelson/stackInfo/MichelsonStackInfoManagerImplTest.kt
@@ -1,13 +1,7 @@
 package com.tezos.lang.michelson.stackInfo
 
-import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.FileEditorManagerEvent
-import com.intellij.openapi.fileEditor.FileEditorManagerListener
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
-import com.tezos.intellij.settings.TezosClientConfig
-import com.tezos.intellij.settings.TezosSettingService
 import com.tezos.lang.michelson.MichelsonFixtureTest
 import com.tezos.lang.michelson.lang.MichelsonFileType
 import org.junit.Assert
@@ -19,66 +13,49 @@ class MichelsonStackInfoManagerImplTest : MichelsonFixtureTest() {
     // no client at first, document edits must show errors in that case
     // the stack info must be refreshed when the client changed, even without changes to the document
     fun testDefaultClientChange() {
-        try {
-            val mockMgr = MockMichelsonStackInfoManager.getInstance(project)
+        val mockMgr = MockMichelsonStackInfoManager.getInstance(project)
 
-            val unfocusedFile = myFixture.configureByText(MichelsonFileType, "")
-            val unfocusedDoc = myFixture.editor.document
-            // close it because an update to the default client will trigger stack updates for selected files
-            FileEditorManager.getInstance(project).closeFile(unfocusedFile.virtualFile)
+        val unfocusedFile = myFixture.configureByText(MichelsonFileType, "")
+        val unfocusedDoc = myFixture.editor.document
+        // close it because an update to the default client will trigger stack updates for selected files
+        FileEditorManager.getInstance(project).closeFile(unfocusedFile.virtualFile)
 
-            // make sure that no client is available
-            TezosSettingService.getSettings().clients.clear()
+        configureByCodeAndFocus("<caret>DROP;")
+        Assert.assertEquals("without a default client opening must not trigger a stack update", 0, mockMgr.documentUpdateCount(myFixture.editor.document))
+        Assert.assertEquals("without a default client opening must not trigger a stack update", 0, mockMgr.documentUpdateCount(unfocusedDoc))
 
-            configureByCodeAndFocus("<caret>DROP;")
-            Assert.assertEquals("without a default client opening must not trigger a stack update", 0, mockMgr.documentUpdateCount(myFixture.editor.document))
-            Assert.assertEquals("without a default client opening must not trigger a stack update", 0, mockMgr.documentUpdateCount(unfocusedDoc))
+        myFixture.type("DROP; ")
+        Assert.assertEquals("without a default client a change must not trigger a stack update", 0, mockMgr.documentUpdateCount(myFixture.editor.document))
+        Assert.assertEquals("without a default client a change must not trigger a stack update", 0, mockMgr.documentUpdateCount(unfocusedDoc))
 
-            myFixture.type("DROP; ")
-            Assert.assertEquals("without a default client a change must not trigger a stack update", 0, mockMgr.documentUpdateCount(myFixture.editor.document))
-            Assert.assertEquals("without a default client a change must not trigger a stack update", 0, mockMgr.documentUpdateCount(unfocusedDoc))
+        // now set our new default client and check again
+        setDefaultClient()
 
-            // now set our new default client and check again
-            setDefaultClient()
-
-            Assert.assertEquals("A new default client must trigger a stack upddate for the active editor", 1, mockMgr.documentUpdateCount(myFixture.editor.document))
-            Assert.assertEquals("A new default client must not trigger updates for unfocused files", 0, mockMgr.documentUpdateCount(unfocusedDoc))
-        } finally {
-            TezosSettingService.getSettings().setClients(emptyList())
-        }
+        Assert.assertEquals("A new default client must trigger a stack upddate for the active editor", 1, mockMgr.documentUpdateCount(myFixture.editor.document))
+        Assert.assertEquals("A new default client must not trigger updates for unfocused files", 0, mockMgr.documentUpdateCount(unfocusedDoc))
     }
 
     fun testUpdateOnFocusChange() {
-        try {
-            val mockMgr = MockMichelsonStackInfoManager.getInstance(project)
+        val mockMgr = MockMichelsonStackInfoManager.getInstance(project)
 
-            val unfocusedFile = myFixture.configureByText(MichelsonFileType, "")
-            val unfocusedDoc = myFixture.editor.document
-            // close it because an update to the default client will trigger stack updates for selected files
-            FileEditorManager.getInstance(project).closeFile(unfocusedFile.virtualFile)
+        val unfocusedFile = myFixture.configureByText(MichelsonFileType, "")
+        val unfocusedDoc = myFixture.editor.document
+        // close it because an update to the default client will trigger stack updates for selected files
+        FileEditorManager.getInstance(project).closeFile(unfocusedFile.virtualFile)
 
-            // after the unfocused file was opened
-            setDefaultClient()
+        // after the unfocused file was opened
+        setDefaultClient()
 
-            configureByCodeAndFocus("<caret>DROP;")
-            val focusedDoc = myFixture.editor.document
+        configureByCodeAndFocus("<caret>DROP;")
+        val focusedDoc = myFixture.editor.document
 
-            Assert.assertEquals(1, mockMgr.documentUpdateCount(focusedDoc))
-            Assert.assertEquals(0, mockMgr.documentUpdateCount(unfocusedDoc))
+        Assert.assertEquals(1, mockMgr.documentUpdateCount(focusedDoc))
+        Assert.assertEquals(0, mockMgr.documentUpdateCount(unfocusedDoc))
 
-            openFileFocused(unfocusedFile)
+        openFileFocused(unfocusedFile)
 
-            Assert.assertEquals(1, mockMgr.documentUpdateCount(focusedDoc))
-            Assert.assertEquals(1, mockMgr.documentUpdateCount(unfocusedDoc))
-        } finally {
-            TezosSettingService.getSettings().setClients(emptyList())
-        }
-    }
-
-    private fun configureByCodeAndFocus(text: String) {
-        configureByCode(text)
-
-        triggerOptionalEditorEvent(myFixture.file.virtualFile)
+        Assert.assertEquals(1, mockMgr.documentUpdateCount(focusedDoc))
+        Assert.assertEquals(1, mockMgr.documentUpdateCount(unfocusedDoc))
     }
 
     private fun openFileFocused(file: PsiFile) {
@@ -86,22 +63,4 @@ class MichelsonStackInfoManagerImplTest : MichelsonFixtureTest() {
         // older builds don't seem to trigger the message on the message bus
         triggerOptionalEditorEvent(file.virtualFile)
     }
-
-    private fun triggerOptionalEditorEvent(file: VirtualFile?) {
-        val newEditor = file?.let {
-            FileEditorManager.getInstance(project).getSelectedEditor(it)
-        }
-
-        if (ideBuildSkipsEditorEvents()) {
-            val event = FileEditorManagerEvent(FileEditorManager.getInstance(project), null, null, file, newEditor)
-            project.messageBus.syncPublisher(FileEditorManagerListener.FILE_EDITOR_MANAGER).selectionChanged(event)
-        }
-    }
-
-    private fun ideBuildSkipsEditorEvents() = ApplicationInfo.getInstance().build.baselineVersion <= 181
-}
-
-private fun setDefaultClient() {
-    TezosSettingService.getSettings().setClients(listOf(TezosClientConfig("test default", "/home/user/alphanet.sh", true)))
-    TezosSettingService.publishDefaultClientChanged()
 }
