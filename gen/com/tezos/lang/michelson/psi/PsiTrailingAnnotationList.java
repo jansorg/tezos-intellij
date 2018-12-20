eff6297ac714d7726f15a859d638612170295eee
jansorg <github@joachim-ansorg.de>
2018-12-20 10:32:43 +0100

    add missing psi element

---
+++ gen/com/tezos/lang/michelson/psi/PsiTrailingAnnotationList.java
@@ -0,0 +1,17 @@
+// This is a generated file. Not intended for manual editing.
+package com.tezos.lang.michelson.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface PsiTrailingAnnotationList extends MichelsonComposite {
+
+  @NotNull
+  List<PsiAnnotation> getAnnotations();
+
+  //WARNING: findParentInstruction(...) is skipped
+  //matching findParentInstruction(PsiTrailingAnnotationList, ...)
+  //methods are not found in MichelsonPsiUtil
+
+}
